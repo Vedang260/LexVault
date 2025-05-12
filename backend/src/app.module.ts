a59061f4ee379faf2eaade6827d471d4d433c95e
Vedang <vedang.wappnet@gmail.com>
2025-05-12 09:52:07 +0530

    added login dto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/user/modules/user.module';
+import { AuthModule } from './modules/auth/modules/auth.module';
+import { typeOrmConfig } from './config/database.config';
+import { AuthMiddleware } from './modules/auth/middlewares/auth.middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    UsersModule,
+  ],
+
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(AuthMiddleware)
+    .exclude(
+      { path: 'api/webhooks/stripe', method: RequestMethod.ALL }, // ✅ Exclude webhook
+    )
+    .forRoutes(
+      { path: 'users', method: RequestMethod.ALL },
+    );
+  }
+}
